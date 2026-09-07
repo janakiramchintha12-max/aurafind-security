202843dc3d8f0de41bcc95b6f0dffab0303468dd
Janaki <janaki@theft.in>
2026-09-07 12:29:47 +0530

    Enable Android 14 microphone permissions and compile updated APK for real-time voice intercom

--- android/app/src/main/java/com/findmydevice/security/service/LocationService.kt
+++ android/app/src/main/java/com/findmydevice/security/service/LocationService.kt
@@ -129,7 +129,9 @@ class LocationService : Service() {
             startForeground(
                 1001,
                 notification,
-                android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION or android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA
+                android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION or 
+                android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA or
+                android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE
             )
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             startForeground(
